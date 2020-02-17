7a14f49aea549cd01dee3e77f79865004235abe5
Neeraj Verma <hello@smurfpandey.me>
2020-02-18 00:21:11 +0530

    save post in DB - Done!

--- insta-archiver.py
+++ insta-archiver.py
@@ -25,49 +25,56 @@ class User(BaseModel):
     profile_url = CharField()
 
 class Post(BaseModel):
-    user = ForeignKeyField(User, backref='tweets')
+    short_code = CharField(primary_key=True)
+    user = ForeignKeyField(User, backref='posts')
     caption = TextField()
+    post_url = CharField()
     created_date = DateTimeField(default=datetime.datetime.now)
-
-class PostMedia(BaseModel):
-    user = ForeignKeyField(User, backref='tweets')
-    message = TextField()
-    created_date = DateTimeField(default=datetime.datetime.now)
-    is_published = BooleanField(default=True)
 
 db.connect()
-db.create_tables([User, Post, PostMedia])
+db.create_tables([User, Post])
 
 # Get instance
 Archiver = Instaloader()
 
 profile_name = "ttt_official"
+profile_url = "https://instagram.com/" + profile_name
+dbUser, created = User.get_or_create(
+    username=profile_name,
+    profile_url=profile_url)
 
-profile = Profile.from_username(Archiver.context, profile_name)
+profile = Profile.from_username(Archiver.context, dbUser.username)
 
-for post in profile.get_posts():
-    print("Got post: https://instagram.com/p/" + post.shortcode)
-
+for instaPost in profile.get_posts():
     # Check if we have already archived this post
-    # If yes: break
-    # If no: proceed
-
-    downloaded = Archiver.download_post(post, target=profile_name)
-    if not downloaded:
+    dbPost = Post.get_or_none(Post.short_code == instaPost.shortcode)
+    if dbPost is not None:
+        print("I have saved this post! I am going! Bye!")
         break
+        
+    postCaption = ""
+    downloaded = Archiver.download_post(instaPost, target=profile_name)
+    for f in listdir(profile_name):
+        file_path = profile_name + '/' + f
+        file_extension = splitext(f)[1]        
+        if file_extension == ".txt":
+            with open(file_path) as yoF:
+                lines = yoF.read()
+                postCaption = lines
+        elif file_extension == ".json" or file_extension == ".xz":
+            unlink(file_path)
+            continue
+        else:
+            print("Found media: " + f)
+        unlink(file_path)
 
+    dbPost = Post.create(
+        short_code=instaPost.shortcode,
+        user=dbUser,
+        caption=postCaption,
+        post_url="https://instagram.com/p/" + instaPost.shortcode,
+        created_date=instaPost.date_utc
+    )
+    print("Post saved?")
 print("And done!")
-    # 
-    # for f in listdir(profile_name):
-    #     file_path = profile_name + '/' + f
-    #     file_extension = splitext(f)[1]        
-    #     if file_extension == ".txt":
-    #         with open(file_path) as yoF:
-    #             lines = yoF.read()
-    #             #print("Caption: " + lines)
-    #     elif file_extension == ".json" or file_extension == ".xz":
-    #         unlink(file_path)
-    #         continue
-    #     else:
-    #         print("Found media: " + f)
-    #     unlink(file_path)
+    
